95cc37e2db86624e8fe500dc961fef3d54f66661
yuya nakamura <nakayuu07@gmail.com>
2019-07-28 21:51:42 +0900

    create reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,20 @@
+const initialState = []
+
+const events = (state = initialState , action) => {
+  switch(action.type) {
+    case 'CREATE_EVENT':
+      const event = { title: action.title, body: action.body}
+      const length = state.length
+      const id = !length ? 1 : state[length - 1].id + 1  
+      return [...state, {id, ...event}]
+    case 'DELETE_EVENT':
+      return state
+    case 'DELETE_ALL_EVENT':
+      return initialState
+    default: 
+      return state
+  }
+}
+
+
+export default events
