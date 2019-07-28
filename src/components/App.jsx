e4ddd47349959388aab2b5754a0a8d8b700a1c25
yuya nakamura <nakayuu07@gmail.com>
2019-07-28 17:14:39 +0900

    introduce bootstrap

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = props => {
   const [state, setState] = useState(props)  
   const {name, price} = state
 
   return (
-    <>
+    <div className="container-fluid">
       <p>現在の{name}は、{price}です</p>
       <button onClick={() => setState({...state, price: price - 100 })}>- 100</button>
       <button onClick={() => setState({...state, price: price + 100})}>+ 100</button>
       <button onClick={() => setState(props)}>reset</button>
       <input value={name} onChange={(e) => setState({...state, name: e.target.value})}/>
-    </>
+    </div>
   )
 }
 
